f15d3cc7f0c09dc3a679ed77d627676388133ea7
Timoromish <timur.tanatarov@vk.com>
2022-11-20 23:06:56 +0500

    removed the initial state todo

--- src/App.js
+++ src/App.js
@@ -5,30 +5,7 @@ import TodoList from "./components/TodoList/TodoList";
 import {useState} from "react";
 
 function App() {
-    const [todo, setTodo] = useState([
-        {
-            id: 1,
-            title: 'Прогуляться с собакой',
-            detail: 'Захватить одноразовый пакетик',
-            status: false,
-        },
-        {
-            id: 2,
-            title: 'Зайти в магазин',
-            detail: 'Захватить авоську',
-            status: false,
-        },
-        {
-            id: 3,
-            title: 'Написать ментору',
-            detail: 'Сказать спасибо за терпение :)',
-            status: false,
-        },
-    ]);
-
-    // let now = `${new Date().toLocaleTimeString()}, ${new Date().toLocaleDateString()}`;
-    // console.log(now);
-
+    const [todo, setTodo] = useState([]);
 
     return (
         <>
